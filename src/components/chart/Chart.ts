1c6438d8a53b06c1bbfb45425f0ca8e7f4ea9105
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-02 19:01:28 -0300

    set default attributes component

--- src/components/chart/Chart.ts
+++ src/components/chart/Chart.ts
@@ -1,13 +1,36 @@
 import { CustomComponent } from "../CustomComponent.js";
 
 export class Chart extends CustomComponent {
+  private dataValues: any[];
+
   constructor() {
     super({ mode: "closed" });
+    this.dataValues = [];
   }
 
-  setComponentAttributes(): void {}
+  setComponentAttributes(): void {
+    const attributesMapping = [
+      { key: "width", value: "600" },
+      { key: "height", value: "300" },
+      { key: "values", value: "[]" },
+    ];
+    attributesMapping.forEach((attrMap) => {
+      if (
+        !this.attributes.getNamedItem(attrMap.key) ||
+        this.attributes.getNamedItem(attrMap.key)?.value === ""
+      ) {
+        const attr = document.createAttribute(attrMap.key);
+        attr.value = attrMap.value;
+        this.attributes.setNamedItem(attr);
+      }
+    });
+  }
 
-  initComponent(): void {}
+  initComponent(): void {
+    this.dataValues = JSON.parse(
+      this.attributes.getNamedItem("values")!.value
+    ) as any[];
+  }
 
   htmlTemplate(): string {
     return `
@@ -18,8 +41,8 @@ export class Chart extends CustomComponent {
     return `
     <style>
       canvas  {
-        width: 300px;
-        height: 300px;
+        width: ${this.attributes.getNamedItem("width")!.value}px;
+        height: ${this.attributes.getNamedItem("height")!.value}px;
         border: 1px solid #777;
       }  
     </style>
